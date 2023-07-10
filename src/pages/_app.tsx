88fc9473454ca3aa3edc429f113dc5767446fb9b
Jake Nichols <jnichols4nrf@gmail.com>
2023-07-10 20:13:54 +0100

    fix: remove custom font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,31 +2,32 @@
 import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
-import localFont from 'next/font/local';
 
-const neueMachina = localFont({
-  src: [
-    {
-      path: '../../public/fonts/NeueMachina-Light.otf',
-      weight: '300',
-      style: 'light',
-    },
-    {
-      path: '../../public/fonts/NeueMachina-Regular.otf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../../public/fonts/NeueMachina-UltraBold.otf',
-      weight: '700',
-      style: 'bold',
-    },
-  ],
-});
+// const neueMachina = localFont({
+//   src: [
+//     {
+//       path: '../../public/fonts/NeueMachina-Light.otf',
+//       weight: '300',
+//       style: 'light',
+//     },
+//     {
+//       path: '../../public/fonts/NeueMachina-Regular.otf',
+//       weight: '400',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../../public/fonts/NeueMachina-UltraBold.otf',
+//       weight: '700',
+//       style: 'bold',
+//     },
+//   ],
+// });
+
+// neueMachina.className
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <div className={neueMachina.className}>
+    <div>
       <Component {...pageProps} />{' '}
     </div>
   );
