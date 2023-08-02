d433974203127193cb84d11da272eb61de48f4c0
Jake Nichols <jnichols4nrf@gmail.com>
2023-08-02 09:19:54 +0100

    fix: remove generated blogs

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 // FIXME: Update this configuration file based on your project information
 
 export const AppConfig = {
-  site_name: 'Homezen Landing',
-  title: 'Homezen Landing',
+  site_name: 'HomeZen Landing',
+  title: 'HomeZen Landing',
   description: 'The new best property app',
   locale: 'en',
 };
